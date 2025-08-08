2b1df90957b262344b59c9a49baec3eab6a03a54
Ainslea Hong <ainslea.hong2028@gmail.com>
2025-08-08 14:34:22 -0400

    many changes

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -1,52 +1,61 @@
 //
-//  ContentView.swift
-//  ToDoList
+// ContentView.swift
+// ToDoList45
 //
-//  Created by Scholar on 8/8/25.
+// Created by Scholar on 8/8/25.
 //
-
 import SwiftUI
 import SwiftData
-
 struct ContentView: View {
-    
-    @State private var showNewTask = false
-    @Query var toDos: [ToDoItem]
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Text("To-Do List")
-                    .font(.system(size: 40))
-                    .fontWeight(.bold)
-                Spacer()
-                Button {
-                    withAnimation {
-                        showNewTask = true
-                    }
-                } label: {
-                    Text("+")
-                        .font(.title)
-                        .fontWeight(.bold)
-                }
-            }//end of Hstack
-            .padding()
-            Spacer()//push up
-            
-            List{//Displays list of tasks
-                ForEach(toDos){toDoItem in Text(toDoItem.title)
-                    
-                }
-            }
-            if showNewTask {
-                NewToDo()
-            }
-            
-        }//end of VStack
-   
+  @State private var showNewTask = false
+  @Query var toDos: [ToDoItem]
+  var body: some View {
+    VStack {
+      HStack{
+        Text("To-Do List")
+          .font(.system(size: 40))
+          .fontWeight(.bold)
+        Spacer()//push left or right
+        Button{
+          withAnimation{
+            showNewTask = true
+          }
+        } label: {
+          Text("+")
+            .font(.title)
+            .fontWeight(.bold)
+        }
+      }//end of the HStack
+      .padding()
+      Spacer()//push up
+      List{//Displays list of tasks
+        ForEach (toDos) { toDoItem in
+          if toDoItem.isImportant {
+            Text(":bangbang:" + toDoItem.title)
+          } else {
+            Text(toDoItem.title)
+          }
+        }
+      }
+      .listStyle(.plain)
+    }//end of the VStack
+    if showNewTask {
+      NewToDo(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
     }
+  }
+}
+#Preview {
+  ContentView()
+    .modelContainer(for: ToDoItem.self, inMemory: true)
 }
 
-#Preview {
-    ContentView()
-}
+
+
+
+
+
+
+
+
+
+
